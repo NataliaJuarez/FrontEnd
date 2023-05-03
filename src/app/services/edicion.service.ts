483916e09b134bc3f1e563e22d7cf6c9a436e44a
NataliaJuarez <juareznatalia94@gmail.com>
2023-05-03 02:02:52 -0300

    Creación de servicio de edición (es correcto??)

---
+++ src/app/services/edicion.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { Observable, of } from 'rxjs'; 
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EdicionService {
+  private apiUrl = 'http://localhost:4200/edit'
+  constructor() { }
+}
