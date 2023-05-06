df53d1db3e6dfe6e6e72b418336e40e8d61105ff
NataliaJuarez <juareznatalia94@gmail.com>
2023-05-05 21:30:35 -0300

    Cambios de html y css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-import {HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-=======
 import { HttpClientModule } from '@angular/common/http';
->>>>>>> iconos
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,11 +13,8 @@ import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { FooterComponent } from './components/footer/footer.component';
-<<<<<<< HEAD
-=======
 import { BotonLoginComponent } from './components/boton-login/boton-login.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
->>>>>>> iconos
 
 
 @NgModule({
@@ -37,22 +28,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     ProjectsComponent,
     SkillsComponent,
     ContactComponent,
-<<<<<<< HEAD
-    FooterComponent
-=======
     FooterComponent,
     BotonLoginComponent
->>>>>>> iconos
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-<<<<<<< HEAD
-    FormsModule
-=======
     FontAwesomeModule
->>>>>>> iconos
   ],
   providers: [],
   bootstrap: [AppComponent]
